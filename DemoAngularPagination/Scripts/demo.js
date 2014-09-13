8292a43c8a4fbd9ed3219c537d180265e0acf375
Davide Guida <mizrael@yahoo.it>
2014-09-13 20:53:40 +0200

    Update demo.js

--- DemoAngularPagination/Scripts/demo.js
+++ DemoAngularPagination/Scripts/demo.js
@@ -13,33 +13,6 @@ demoApp.factory('Product', ['$resource',
       });
   }]);
 
-
-/*************************************************************************/
-// DIRECTIVES
-
-demoApp.directive('demoPager', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'scripts/templates/pager-template.html',
-        controller: ['$scope', function ($scope) {
-            $scope.range = function () {
-                var step = 2;
-                var doubleStep = step * 2;
-                var start = Math.max(0, $scope.page - step);
-                var end = start + 1 + doubleStep;
-                if (end > $scope.pagesCount) { end = $scope.pagesCount; }
-
-                var ret = [];
-                for (var i = start; i != end; ++i) {
-                    ret.push(i);
-                }
-
-                return ret;
-            };
-        }]
-    }
-});
-
 /*************************************************************************/
 // CONTROLLERS
 
@@ -73,3 +46,30 @@ demoApp.controller('ProductsArchiveCtrl', ['$scope', 'Product',
         $scope.search();
     }]
 );
+
+
+/*************************************************************************/
+// DIRECTIVES
+
+demoApp.directive('demoPager', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'scripts/templates/pager-template.html',
+        controller: ['$scope', function ($scope) {
+            $scope.range = function () {
+                var step = 2;
+                var doubleStep = step * 2;
+                var start = Math.max(0, $scope.page - step);
+                var end = start + 1 + doubleStep;
+                if (end > $scope.pagesCount) { end = $scope.pagesCount; }
+
+                var ret = [];
+                for (var i = start; i != end; ++i) {
+                    ret.push(i);
+                }
+
+                return ret;
+            };
+        }]
+    }
+});
